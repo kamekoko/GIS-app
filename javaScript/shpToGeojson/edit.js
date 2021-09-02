1b49960cc3727f6ca974e7a526a7b8573f5611cb
kamekoko <kameko.jpn17@gmail.com>
2021-09-02 18:48:37 +0900

    minor change

--- javaScript/shpToGeojson/edit.js
+++ javaScript/shpToGeojson/edit.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 
+const start = [36.39326604089627, 136.52501053249725];
+const end = [36.37429836513503, 136.52831501381425];
+
+const minLon = Math.min(start[0], end[0]) - 0.01;
+const maxLon = Math.max(start[0], end[0]) + 0.01;
+const minLat = Math.min(start[1], end[1]) - 0.01;
+const maxLat = Math.max(start[1], end[1]) + 0.01;
+
+const bbox = [ //[min lon, max lon][min lat, max lat]
+  [minLon, maxLon],
+  [minLat, maxLat]
+];
+// console.log(bbox);
+
 const jsonObject = JSON.parse(fs.readFileSync('./out3.geojson', 'utf8'));
 
 console.log('{\n\t"type": "FeatureCollection",\n\t "features": [');
@@ -7,10 +21,17 @@ console.log('{\n\t"type": "FeatureCollection",\n\t "features": [');
 var count = 0;
 
 jsonObject.features.forEach((obj) => {
-    count++;
-    if (count > 4) return;
-    console.log(JSON.stringify(obj, null, "  "));
-    console.log(',')
+  // count++;
+  // if (count > 2) return;
+  // console.log(obj.geometry.coordinates[0][0][0] + " , " + obj.geometry.coordinates[0][0][1]);
+
+  if (obj.geometry.coordinates[0][0][0] > minLat &&
+      obj.geometry.coordinates[0][0][0] < maxLat &&
+      obj.geometry.coordinates[0][0][1] > minLon &&
+      obj.geometry.coordinates[0][0][1] < maxLon) {
+        console.log(JSON.stringify(obj, null, "  "));
+        console.log(',');
+    }
 });
 
 console.log(']\n}');
