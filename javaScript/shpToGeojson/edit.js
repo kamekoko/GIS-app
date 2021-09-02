befd8d67158833946d9e2b0cfccf9b706972498a
kamekoko <kameko.jpn17@gmail.com>
2021-09-02 16:30:34 +0900

    minor change

--- javaScript/shpToGeojson/edit.js
+++ javaScript/shpToGeojson/edit.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const jsonObject = JSON.parse(fs.readFileSync('./out2.geojson', 'utf8'));
+const jsonObject = JSON.parse(fs.readFileSync('./out3.geojson', 'utf8'));
 
 console.log('{\n\t"type": "FeatureCollection",\n\t "features": [');
 
@@ -13,6 +13,4 @@ jsonObject.features.forEach((obj) => {
     console.log(',')
 });
 
-console.log()
-
-console.log(']\n"bbox": [\n\t-70469.59017605362,\n\t27964.251346610803,\n\t-52884.54310107909,\n\t49411.407682093384\n]\n}');
+console.log(']\n}');
