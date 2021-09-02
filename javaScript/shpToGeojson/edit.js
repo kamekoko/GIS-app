7dc79c14349437babd93fb788d3148a875effa7e
kamekoko <kameko.jpn17@gmail.com>
2021-09-02 15:38:08 +0900

    minor change

---
+++ javaScript/shpToGeojson/edit.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const jsonObject = JSON.parse(fs.readFileSync('./out2.geojson', 'utf8'));
+
+console.log('{\n\t"type": "FeatureCollection",\n "features": [');
+
+var count = 0;
+
+jsonObject.features.forEach((obj) => {
+    count++;
+    if (count > 100) return;
+    console.log(JSON.stringify(obj, null, "  "));
+});
+
+console.log()
+
+console.log('],\n}');
